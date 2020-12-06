4aa75a7257af16af78e34d6e296ea95ac8f8241c
Trallerd <jessicacristec@gmail.com>
2020-12-06 17:44:42 -0300

    The Bank Web

--- app/src/main/java/com/trallerd/thebank/dao/RecordsDAO.kt
+++ app/src/main/java/com/trallerd/thebank/dao/RecordsDAO.kt
@@ -1,8 +1,11 @@
 package com.trallerd.thebank.dao
 
 import android.util.Log
+import android.widget.Toast
+import com.trallerd.thebank.MainActivity
 import com.trallerd.thebank.models.Records
 import com.trallerd.thebank.network.services.RecordsService
+import kotlinx.coroutines.newSingleThreadContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -10,43 +13,35 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RecordsDAO {
-    val recordsRetrofit = Retrofit.Builder()
-        .baseUrl(" http://192.168.100.32:3000/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    val recordsRetrofit = Retrofit.Builder().baseUrl(" http://192.168.100.32:3000/").addConverterFactory(GsonConverterFactory.create()).build()
     val recordsService = recordsRetrofit.create(RecordsService::class.java)
 
-    fun getAllById(id: Long, finished: (List<Records>) -> Unit) {
+    fun getAllById(id: Long,finished: (List<Records>) -> Unit) {
         recordsService.getAllById(id).enqueue(object : Callback<List<Records>> {
-            override fun onResponse(call: Call<List<Records>>, response: Response<List<Records>>) {
+            override fun onResponse(call: Call<List<Records>>,response: Response<List<Records>>) {
                 if (response.body() != null) {
                     val records = response.body()
                     finished(records!!)
-                }else{
-                    Log.i("TOAST", response.body().toString())
                 }
             }
 
-            override fun onFailure(call: Call<List<Records>>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<List<Records>>,t: Throwable) {
+
             }
 
         })
     }
 
-    fun getRecordById(id: Long, idRec: Long, finished: (Records) -> Unit) {
-        recordsService.getRecordById(id, idRec).enqueue(object : Callback<Records> {
-            override fun onResponse(call: Call<Records>, response: Response<Records>) {
+    fun getRecordById(id: Long,idRec: Long,finished: (Records) -> Unit) {
+        recordsService.getRecordById(id,idRec).enqueue(object : Callback<Records> {
+            override fun onResponse(call: Call<Records>,response: Response<Records>) {
                 if (response.body() != null) {
                     val records = response.body()
                     finished(records!!)
-                }else{
-                    Log.i("TOAST", response.body().toString())
                 }
             }
 
-            override fun onFailure(call: Call<Records>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<Records>,t: Throwable) {
             }
 
         })
@@ -54,84 +49,74 @@ class RecordsDAO {
 
     fun getAll(finished: (List<Records>) -> Unit) {
         recordsService.getAll().enqueue(object : Callback<List<Records>> {
-            override fun onResponse(call: Call<List<Records>>, response: Response<List<Records>>) {
+            override fun onResponse(call: Call<List<Records>>,response: Response<List<Records>>) {
                 if (response.body() != null) {
                     val records = response.body()
                     finished(records!!)
-                }else{
-                    Log.i("TOAST", response.body().toString())
                 }
+
             }
 
-            override fun onFailure(call: Call<List<Records>>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<List<Records>>,t: Throwable) {
             }
 
         })
 
     }
 
-    fun getAllByName(name: String, finished: (List<Records>) -> Unit) {
+    fun getAllByName(name: String,finished: (List<Records>) -> Unit) {
         recordsService.getAllByName(name).enqueue(object : Callback<List<Records>> {
-            override fun onResponse(call: Call<List<Records>>, response: Response<List<Records>>) {
+            override fun onResponse(call: Call<List<Records>>,response: Response<List<Records>>) {
                 if (response.body() != null) {
                     val records = response.body()
                     finished(records!!)
-                }else{
-                    Log.i("TOAST", response.body().toString())
                 }
             }
 
-            override fun onFailure(call: Call<List<Records>>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<List<Records>>,t: Throwable) {
             }
         })
 
 
     }
-    fun insert(records: Records, finished: (Records) -> Unit) {
+
+    fun insert(records: Records,finished: (Records) -> Unit) {
         recordsService.insert(records).enqueue(object : Callback<Records> {
-            override fun onResponse(call: Call<Records>, response: Response<Records>) {
+            override fun onResponse(call: Call<Records>,response: Response<Records>) {
                 if (response.body() != null) {
                     val records = response.body()
                     finished(records!!)
-                }else{
-                    Log.i("TOAST", response.body().toString())
                 }
             }
 
-            override fun onFailure(call: Call<Records>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<Records>,t: Throwable) {
             }
         })
 
     }
 
-    fun update(id: Long, records: Records, finished: (Records) -> Unit) {
-        recordsService.update(id, records).enqueue(object : Callback<Records> {
-            override fun onResponse(call: Call<Records>, response: Response<Records>) {
+    fun update(id: Long,records: Records,finished: (Records) -> Unit) {
+        recordsService.update(id,records).enqueue(object : Callback<Records> {
+            override fun onResponse(call: Call<Records>,response: Response<Records>) {
                 if (response.body() != null) {
                     val records = response.body()
                     finished(records!!)
-                }else{
-                    Log.i("TOAST", response.body().toString())
                 }
+
             }
 
-            override fun onFailure(call: Call<Records>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<Records>,t: Throwable) {
             }
         })
     }
 
-    fun delete(id: Long, finished: () -> Unit) {
-        recordsService.delete(id).enqueue(object : Callback<Void>{
-            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+    fun delete(id: Long,finished: () -> Unit) {
+        recordsService.delete(id).enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>,response: Response<Void>) {
                 finished()
             }
 
-            override fun onFailure(call: Call<Void>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<Void>,t: Throwable) {
             }
         })
     }
--- app/src/main/java/com/trallerd/thebank/dao/UserDAO.kt
+++ app/src/main/java/com/trallerd/thebank/dao/UserDAO.kt
@@ -10,58 +10,51 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class UserDAO {
-    val userRetrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.100.32:3000/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    val userRetrofit = Retrofit.Builder().baseUrl("http://192.168.100.32:3000/").addConverterFactory(GsonConverterFactory.create()).build()
     val userService = userRetrofit.create(UserService::class.java)
 
 
-    fun getUser(username: String, password: String, finished: (Users) -> Unit) {
-        userService.getUser(username, password).enqueue(object : Callback<List<Users>> {
-            override fun onResponse(call: Call<List<Users>>, response: Response<List<Users>>) {
+    fun getUser(username: String,password: String,finished: (Users) -> Unit) {
+        userService.getUser(username,password).enqueue(object : Callback<List<Users>> {
+            override fun onResponse(call: Call<List<Users>>,response: Response<List<Users>>) {
                 if (response.body() != null) {
                     val users = response.body()!!.first()
                     finished(users)
-                } 
+                }
             }
 
-            override fun onFailure(call: Call<List<Users>>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<List<Users>>,t: Throwable) {
             }
 
 
         })
     }
 
-    fun insert(user: Users, finished: (Users) -> Unit) {
+    fun insert(user: Users,finished: (Users) -> Unit) {
         userService.insert(user).enqueue(object : Callback<Users> {
-            override fun onResponse(call: Call<Users>, response: Response<Users>) {
+            override fun onResponse(call: Call<Users>,response: Response<Users>) {
                 if (response.body() != null) {
                     val users = response.body()
                     finished(users!!)
-                } else {
-                    Log.i("TOAST", response.body().toString())
                 }
+
 
             }
 
-            override fun onFailure(call: Call<Users>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<Users>,t: Throwable) {
             }
 
         })
 
     }
 
-    fun delete(user: Users, finished: () -> Unit) {
+    fun delete(user: Users,finished: () -> Unit) {
         userService.delete(user.id!!).enqueue(object : Callback<Void> {
-            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+            override fun onResponse(call: Call<Void>,response: Response<Void>) {
                 finished()
             }
 
-            override fun onFailure(call: Call<Void>, t: Throwable) {
-                Log.i("TOAST", t.toString())
+            override fun onFailure(call: Call<Void>,t: Throwable) {
             }
 
         })
